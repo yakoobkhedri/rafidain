df2b4f20e3f4e341ebe1ea968c8e7662958e340a
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-04-23 23:54:53 +0330

    commit message

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,10 @@
 import { createStore } from 'vuex';
-import products from './modules/products'
-import categories from './modules/category'
 import articles from './modules/articles'
-import cart from './modules/cart'
 
 
 const store = createStore({
    modules:{
-      products,
       articles,
-      categories,
-      cart
    }
 });
 
